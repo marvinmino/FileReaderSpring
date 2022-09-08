7a3df0a363e97796f58e0ec78918e889faf247b0
Marvin Mino <marvinmino1@gmail.com>
2022-09-08 16:52:30 +0200

    removed unused code

--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -14,13 +14,6 @@ import java.util.jar.Attributes;
 public class HomeController {
     @GetMapping
     public String home(Model model) {
-        model.addAttribute("text","Thymeleaf first text");
        return "Home";
-    }
-
-    @GetMapping("/file-data")
-    public String FileReader(Model model, RedirectAttributes attributes) {
-        model.addAttribute("text","Thymeleaf first text");
-        return "FileReader";
     }
 }
